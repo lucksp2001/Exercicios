f372083fbb859f5a0933100f891cdfee53e261e7
Lucas Cardozo <lucasbrasil453@gmail.com>
2022-03-27 16:41:37 -0300

    Salvando as alterações

--- Java/Java_Basico/ex001/src/Ex001.java
+++ Java/Java_Basico/ex001/src/Ex001.java
@@ -14,8 +14,20 @@ public class Ex001 {
 
 	/**
 	 * @param args
+
 	 */
+	
+	/*Descrição do Exercício:
+	 * 
+	 * 1. Faça um algoritmo que leia a idade de uma pessoa expressa em anos, 
+	 * meses e dias e mostre-a expressa em dias. Leve em consideração o ano 
+	 * com 365 dias e o mês com 30. 
+	 * (Ex: 3 anos, 2 meses e 15 dias = 1170 dias.) 
+	 * 
+	 * */
+	
 	public static void main(String[] args) {
+		
 		Scanner ler = new Scanner(System.in);
 		
 		System.out.print("Digite sua idade em dias: ");
